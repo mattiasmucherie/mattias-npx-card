c247f7d9e0e5a83b6c817eb06d4c2918a2626f47
Mattias Mucherie <mattias.mattias.m@gmail.com>
2021-08-27 09:55:26 +0200

    Updated workplace

--- build.js
+++ build.js
@@ -1,51 +1,56 @@
-'use strict'
+"use strict";
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
-const fs = require('fs')
-const path = require('path')
+const chalk = require("chalk");
+const boxen = require("boxen");
+const fs = require("fs");
+const path = require("path");
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
-}
+  borderStyle: "round",
+};
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Mattias Mucherie'),
-  handle: chalk.white('mattiasmucherie'),
-  work: chalk.white('Frontend Developer at Scania CV AB'),
-  github: chalk.gray('https://github.com/') + chalk.green('mattiasmucherie'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('mattias-mucherie'),
-  web: chalk.cyan('https://mattiasmucherie.com'),
+  name: chalk.white("               Mattias Mucherie"),
+  handle: chalk.white("mattiasmucherie"),
+  work: chalk.white("Frontend Developer at tretton37"),
+  github: chalk.gray("https://github.com/") + chalk.green("mattiasmucherie"),
+  linkedin:
+    chalk.gray("https://linkedin.com/in/") + chalk.blue("mattias-mucherie"),
+  web: chalk.cyan("https://mattiasmucherie.com"),
 
-
-  labelWork: chalk.white.bold('       Work:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-
-}
+  labelWork: chalk.white.bold("       Work:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} / ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-
+const newline = "\n";
+const heading = `${data.name} / ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing  // data.labelWeb + data.web
+const output =
+  heading + // data.name + data.handle
+  newline +
+  newline + // Add one whole blank line
+  working +
+  newline + // data.labelWork + data.work
+  githubing +
+  newline + // data.labelGitHub + data.github
+  linkedining +
+  newline + // data.labelLinkedIn + data.linkedin
+  webing; // data.labelWeb + data.web
 
-
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(
+  path.join(__dirname, "bin/output"),
+  chalk.green(boxen(output, options))
+);
